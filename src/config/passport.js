fda191b58e9997346a21d12637724d1380cc1023
Mougni-Nawel <mougninawel@gmail.com>
2025-08-24 16:06:08 +0200

    adding .env.sample and mofication of pipeline, clean code

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport'); // Add this import
 const { findOrCreateGoogleUser } = require('../services/googleAuthService');
 
-// config/passport.js
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
@@ -9,7 +8,6 @@ passport.use(new GoogleStrategy({
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, done) => {
-  console.log('Google profile:', profile); // Add this for debugging
   try {
     const user = await findOrCreateGoogleUser(profile);
     return done(null, user);
